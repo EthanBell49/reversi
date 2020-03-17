b3f0b03af4882e8b142d91f7c85134a367ee2ace
Ethan Bell <etbell@westmont.edu>
2020-03-16 23:42:49 -0700

    socket

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+/****************************************************************************/
+/*    set up the static file server      */
 /* include the static file webserver libary */
 var static = require('node-static');
 
@@ -29,3 +31,22 @@ var app = http.createServer(
     ).listen(port);
 
 console.log('The server is running');
+/***************************************************************/
+/*           set up the web socket server                   */
+var io = require('socket.io').listen(app);
+io.sockets.on('connection', function (socket){
+  function log(){
+    var array = ['*** Server Log Message: '];
+    for(var i = 0; i < arguments.length; i++){
+      array.push(arguments[i]);
+      console.log(arguments[i]);
+    }
+    socket.emit('log',array);
+    socket.broadcast.emit('log',array);
+  }
+  log('A web site connected to the server');
+  
+  socket.on('disconnected',function(socket){
+    log('A web site disconnected from the server');
+  });
+});
