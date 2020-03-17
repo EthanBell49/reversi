0a8799b5cecaac8f2ce738f55879263ece43e076
Ethan Bell <etbell@westmont.edu>
2020-03-16 22:54:21 -0700

    messege page

---
+++ public/js/main.js
@@ -0,0 +1,20 @@
+/* funtion for general use */
+
+function getURLParameters(whichParam)
+{
+  var pageURL = window.location.search.substring(1);
+  var pageURLVariables = pageURL.split('&');
+  for(var i = 0; i < pageURLVariables.length; i++){
+    var paramterName = pageURLVariables[i].split('=');
+    if (paramterName[0] == whichParam) {
+      return paramterName[1];
+    }
+  }
+}
+
+var username = getURLParameters('username');
+if ('undefined' == typeof username || !username) {
+  username = 'Anonymous_'+Math.random();
+}
+
+$('#messages').append('<h4>'+username+'</h4>');
