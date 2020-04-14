0fd57a123804a5c5fb79e639be75701d8d0c2357
Ethan Bell <etbell@westmont.edu>
2020-04-14 04:57:27 -0700

    week 13 done

--- public/js/main.js
+++ public/js/main.js
@@ -265,6 +265,9 @@ $(function(){
 
   console.log('*** Client Log Message: \'join_room\' payload: '+JSON.stringify(payload));
   socket.emit('join_room', payload);
+
+  $('#quit').append('<a href="lobby.html?username='+username+'" class="btn btn-danger btn-default active" role="button" aria-pressed="true">Quit</a>');
+
 });
 
 var old_board = [
@@ -311,9 +314,18 @@ socket.on('game_update', function(payload){
 
     /*animate canges to the board */
 
+    var blacksum = 0;
+    var whitesum = 0;
     var row,column;
     for(row = 0; row < 8; row++){
       for(column = 0; column < 8; column++){
+        if(board[row][column] == 'b'){
+          blacksum++
+        }
+        if(board[row][column] == 'w'){
+          whitesum++
+        }
+
         /* if a board space has changed */
         if(old_board[row][column] != board [row][column]){
           if(old_board[row][column] == '?' && board [row][column] == ' '){
@@ -371,6 +383,8 @@ socket.on('game_update', function(payload){
         }
       }
     }
+    $('#blacksum').html(blacksum);
+    $('#whitesum').html(whitesum);
     old_board = board;
 });
 
@@ -383,4 +397,19 @@ socket.on('play_token_response', function(payload){
         alert(payload.message);
         return;
     }
+});
+
+
+socket.on('game_over', function(payload){
+
+    console.log('*** Client Log Message: \'game_over\'\n\tpayload: '+JSON.stringify(payload));
+    if(payload.result == 'fail'){
+        console.log(payload.message);
+        return;
+    }
+    /* jump to new page */
+
+    $('#game_over').html('<h1>Game Over</h1>,<h2>'+payload.who_won+' won!</h2>');
+    $('#game_over').append('<a href="lobby.html?username='+username+'" class="btn btn-success btn-lg active" role="button" aria-pressed="true">Return to the lobby</a>');
+
 });
